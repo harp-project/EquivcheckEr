597f3cf8fbff82834d250cbc75b54ec455aa097d
Bendeguz Seres <grafitpor@gmail.com>
2022-12-11 09:10:04 +0100

    Add functions for finding the callers of some function

--- src/general_refac.erl
+++ src/general_refac.erl
@@ -1,7 +1,21 @@
 -module(general_refac).
+
 -compile(export_all). % Exports all functions
 -compile(debug_info).
 
 diffing(OrigHash, RefacHash) ->
     Out = os:cmd("git diff --no-ext-diff " ++ OrigHash ++ " " ++ RefacHash ++ " | \grep -o '^[-+][^[:space:]]*([^->]*)' | tr -d '+-'"),
     string:split(string:trim(Out), "\n").
+
+get_arity(FunStr) ->
+    [_ , ArgStr] = string:split(FunStr, "("),
+    length(string:split(ArgStr, ",", all)).
+
+get_filename(FunName) ->
+    string:trim(os:cmd("grep -l '^" ++ FunName ++ "' *")).
+
+find_callers(FunName, Arity) ->
+    FileName = get_filename(FunName),
+    FunNameAtom = list_to_atom(FunName),
+    {_, Funs} = wrangler_code_inspector_lib:calls_to_fun_1(FileName, FunNameAtom, Arity, ["."], 4),
+    Funs.
