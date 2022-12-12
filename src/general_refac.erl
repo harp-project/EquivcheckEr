55f1da1b1f76a6cead074486bc16bd8b91bf84e0
Bendeguz Seres <grafitpor@gmail.com>
2022-12-12 07:59:35 +0100

    Change data formats

--- src/general_refac.erl
+++ src/general_refac.erl
@@ -3,9 +3,21 @@
 -compile(export_all). % Exports all functions
 -compile(debug_info).
 
+% These all have to be called from the directory of the source code!
+
 diffing(OrigHash, RefacHash) ->
     Out = os:cmd("git diff --no-ext-diff " ++ OrigHash ++ " " ++ RefacHash ++ " | \grep -o '^[-+][^[:space:]]*([^->]*)' | tr -d '+-'"),
-    string:split(string:trim(Out), "\n").
+    FunStrs = string:split(string:trim(Out), "\n"),
+    lists:map(fun(X) -> format(X) end, FunStrs).
+
+format(FunStr) ->
+    [Name , ArgStr] = string:split(FunStr, "("),
+    Arit = length(string:split(ArgStr, ",", all)),
+    {Name, Arit}.
+
+get_name(FunStr) ->
+    [Name , _] = string:split(FunStr, "("),
+    Name.
 
 get_arity(FunStr) ->
     [_ , ArgStr] = string:split(FunStr, "("),
@@ -14,8 +26,10 @@ get_arity(FunStr) ->
 get_filename(FunName) ->
     string:trim(os:cmd("grep -l '^" ++ FunName ++ "' *")).
 
-find_callers(FunName, Arity) ->
+% TODO Doesn't work with nested files
+find_callers({FunName, Arity}) ->
     FileName = get_filename(FunName),
     FunNameAtom = list_to_atom(FunName),
-    {_, Funs} = wrangler_code_inspector_lib:calls_to_fun_1(FileName, FunNameAtom, Arity, ["."], 4),
-    Funs.
+    {_, Folder} = file:get_cwd(),
+    {_, Funs} = wrangler_code_inspector_lib:calls_to_fun_1(FileName, FunNameAtom, Arity, [Folder], 4),
+    lists:map(fun({{FileName,_,_},_}) -> FileName end, Funs).
