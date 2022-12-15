d34e44846f478f48bab578b6d131985ed24a1e94
Bendeguz Seres <grafitpor@gmail.com>
2022-12-15 07:32:46 +0100

    Helpers for demo

--- src/utils.erl
+++ src/utils.erl
@@ -9,10 +9,16 @@ demo2() ->
     debugger:quick(vsc_equiv, check_equiv, ["79207c7c3", "5d434d", f_old, f_new]).
 
 demo3() ->
-    vsc_equiv:check_equiv("468f49", "0f07c3a", f_old, f_new).
+    vsc_equiv:check_equiv("468f49", "be6b6", f_old, f_new).
 
 demo3_debug() ->
-    debugger:quick(vsc_equiv, check_equiv, ["468f49", "0f07c3a", f_old, f_new]).
+    debugger:quick(vsc_equiv, check_equiv, ["468f49", "be6b6", g, g]).
+
+demo4() ->
+    debugger:quick(vsc_equiv, check_equiv, ["468f4", "be6b6"]).
+
+demo5() ->
+    debugger:quick(vsc_equiv, check_equiv, ["6f5871924ea", "2c5911a7847b"]).
 
 bench(Count) ->
     run(Count) / Count.
