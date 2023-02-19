48352601faba2146438e09feafd3f1a928d8aa6e
Bendeguz Seres <grafitpor@gmail.com>
2023-02-19 08:22:25 +0100

    Add module for managing configs

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 TARGET_DIR=ebin
 
-COMPILE_OPTIONS=+debug_info
+COMPILE_OPTIONS=+debug_info +export_all
 TEST_COMPILE_OPTIONS=+debug_info +export_all
 
 all: compile
@@ -10,6 +10,7 @@ compile:
 	erlc ${COMPILE_OPTIONS} -o ${TARGET_DIR} src/utils.erl
 	erlc ${COMPILE_OPTIONS} -o ${TARGET_DIR} src/typing.erl
 	erlc ${COMPILE_OPTIONS} -o ${TARGET_DIR} src/scoping.erl
+	erlc ${COMPILE_OPTIONS} -o ${TARGET_DIR} src/config.erl
 	# erl -sname master -eval 'check_equiv:main(), init:stop()' -noshell
 
 test: FORCE
@@ -19,5 +20,6 @@ test: FORCE
 	erlc ${TEST_COMPILE_OPTIONS} -o ${TARGET_DIR} src/scoping.erl
 	erlc ${TEST_COMPILE_OPTIONS} -o ${TARGET_DIR} test/scoping_tests.erl
 	erlc ${TEST_COMPILE_OPTIONS} -o ${TARGET_DIR} test/typing_tests.erl
+	erlc ${TEST_COMPILE_OPTIONS} -o ${TARGET_DIR} src/config.erl
 	erl -eval 'scoping_tests:test(), typing_tests:test(), init:stop()' -noshell
 FORCE:
---
+++ src/config.erl
@@ -0,0 +1,47 @@
+-module(config).
+
+-export([load_config/0,save_config/1,update_config/3]).
+
+-type config() :: [{string(), string()}].
+
+-define(CONF_DEFAULT_LOC, "/equivcheckrc").
+
+-spec update_config(config(), string(), string()) -> config().
+update_config(Config, Key, Value) ->
+    lists:keyreplace(Key, 1, Config, {Key, Value}).
+
+-spec create_default() -> config().
+create_default() ->
+    [].
+    
+-spec deserialize(string()) -> config().
+deserialize(ConfigStr) ->
+    Lines = string:split(string:trim(ConfigStr), "\n", all),
+    lists:map(fun(Line) ->
+                      [Key, Value] = string:split(Line, " "),
+                      {Key, Value} end, Lines).
+
+-spec serialize(config()) -> string().
+serialize(Config) ->
+    lists:foldr(fun({Key, Value}, ConfigStr) ->
+                        Key ++ " " ++ Value ++ "\n" ++ ConfigStr end,
+                "",
+                Config).
+
+-spec load_config() -> config().
+load_config() -> % TODO non-default location as argument
+    ConfigDir = os:getenv("XDG_CONFIG_HOME"),
+    FileName = ConfigDir ++ ?CONF_DEFAULT_LOC,
+    case filelib:is_file(FileName) of
+        true ->
+            {_, File} = file:read_file(FileName),
+            deserialize(binary:bin_to_list(File));
+        false ->
+            create_default()
+    end.
+
+-spec save_config(config()) -> atom().
+save_config(Config) ->
+    ConfigDir = os:getenv("XDG_CONFIG_HOME"),
+    FileName = ConfigDir ++ ?CONF_DEFAULT_LOC,
+    file:write_file(FileName, binary:list_to_bin(serialize(Config))).
