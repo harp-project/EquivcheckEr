5941da413a2ec754367fda014017c22f1a17c213
Bendeguz Seres <grafitpor@gmail.com>
2023-05-29 07:38:48 +0200

    Add improvements for finding functions

--- src/functions.erl
+++ src/functions.erl
@@ -1,45 +1,61 @@
 -module(functions).
--compile(export_all).
 
--type source() :: [string()].
--type filename() :: string().
--type fun_info() :: {atom(), string(), integer()}.
--type line_info() :: {integer(), integer()}.
--type boundaries() :: {fun_info(), line_info()}.
+-export([functions/2]).
 
+-type fun_name() :: string().
+-type fun_arity() :: integer().
+-type line_num() :: integer().
+-type boundaries() :: {line_num(), line_num()}.
+-type ast() :: erl_syntax:forms().
+-type tokens() :: erl_scan:tokens().
 
-test() ->
-    {ok, F} = file:read_file("diff.erl"),
-    Source = string:split(erlang:binary_to_list(F), "\n", all),
-    boundaries({"diff.erl", {Source, Source}}).
-
--spec boundaries({filename(), {source(), source()}}) -> [{boundaries(), boundaries()}].
-boundaries({FileName, {OrigLines, RefacLines}}) ->
-    OrigFuns = functions(FileName, OrigLines),
-    RefacFuns = functions(FileName, RefacLines),
-    lists:zip(OrigFuns, RefacFuns).
-
-add2({CurrNum, CurrSig}, {NextStart, _, _}) ->
-    [{CurrNum, NextStart - 1, CurrSig}];
-add2({CurrNum, CurrSig}, [{NextStart, NextEnd, NextSig}|T]) ->
-    [{CurrNum, NextStart - 1, CurrSig}, {NextStart, NextEnd, NextSig}|T].
+% Finds all the functions in the AST, and pairs them up with their first and last line in the source
+-spec functions(ast(), tokens()) -> [{fun_name(), fun_arity(), boundaries()}].
+functions(AST, Tokens) ->
+    Functions = lists:filter(fun(Tree) -> erl_syntax:type(Tree) =:= function end, AST),
+    StartLines = lists:map(fun(Tree) -> erl_syntax:get_pos(leftmost_node(Tree)) end, Functions),
+    RightmostLines = lists:map(fun(Tree) -> erl_syntax:get_pos(rightmost_node(Tree)) end, Functions),
+    EndLines = lists:map(fun(EndLine) -> offset(EndLine, Tokens) end, RightmostLines),
+    Names = lists:map(fun(Tree) -> erl_syntax:atom_literal(erl_syntax:function_name(Tree)) end, Functions),
+    Arities = lists:map(fun erl_syntax:function_arity/1, Functions),
+    lists:zip3(Names, Arities, lists:zip(StartLines, EndLines)).
 
 
--spec functions(filename(), source()) -> boundaries().
-functions(FileName, Lines) ->
-    LineNums = lists:seq(1, length(Lines)),
-    IndLines = lists:zip(LineNums, Lines),
-    Signatures = lists:filter(fun({_, Line}) -> diff:is_function_sig(Line) end, IndLines),
-    lists:map(fun({StartLine, EndLine, Line}) -> function(FileName, StartLine, EndLine, Line) end,
-              lists:foldr(fun add2/2, {length(Lines) + 1, 0, ""}, Signatures)).
+-spec rightmost_node(ast()) -> ast().
+rightmost_node(Tree) ->
+    case erlang:is_list(Tree) of
+        true -> rightmost_node(lists:last(Tree));
+        false ->
+            Children = erl_syntax:subtrees(Tree),
+            case lists:reverse(Children) of
+                [] -> Tree;
+                [RightmostChild | _] ->
+                    case RightmostChild of
+                        [] -> Tree;
+                        _  -> rightmost_node(RightmostChild)
+                    end
+            end
+    end.
 
+-spec leftmost_node(ast()) -> ast().
+leftmost_node(Tree) ->
+    case erlang:is_list(Tree) of
+        true -> leftmost_node(hd(Tree));
+        false ->
+            Children = erl_syntax:subtrees(Tree),
+            case Children of
+                [] -> Tree;
+                [LeftMostChild | _] -> leftmost_node(LeftMostChild)
+            end
+    end.
 
-function(FileName, StartLine, EndLine, Line) ->
-    M = slicing:get_module(FileName),
-    {F, A} = diff:get_name_and_arity(Line),
-    {{M, F, A}, {StartLine, EndLine}}.
-
-add({dot,Line}, [H|T]) ->
-    [[],[{dot, Line}|H]|T];
-add(Token, [H|T]) ->
-    [[Token|H]|T].
+% Finds the number of lines after the last meaningful node in the AST
+% This is needed because we want the last line of the function based on the
+% source file, not from the AST, which doesn't deal with dots and other "unnecessary"
+% syntactic elements
+-spec offset(line_num(), tokens()) -> line_num().
+offset(LineNum, Tokens) ->
+    Rest = lists:dropwhile(fun(T) -> element(2, T) =/= LineNum end, Tokens),
+    % We use dot as the last token from the function
+    {value, EndToken} = lists:search(fun(T) -> element(1, T) =:= dot end, Rest),
+    element(2, EndToken).
