75e659d26ab927ff3d4cc16e02f7e5ab4dfd6ea9
Bendeguz Seres <grafitpor@gmail.com>
2023-06-13 09:33:26 +0200

    Move get_module to utils

--- src/utils.erl
+++ src/utils.erl
@@ -44,3 +44,9 @@ bracket([H|[_|T]], Acc, Level) when ([H] =:= "]") and (Level =:= 0) -> [Acc ++ "
 bracket([H|[]], Acc, Level) when ([H] =:= "]") and (Level =:= 0) -> [Acc ++ "]"];
 bracket([H|T], Acc, Level) when ([H] =:= "]") and (Level =/= 0) -> bracket(T, Acc ++ "]", Level - 1);
 bracket([H|T], Acc, Level) -> bracket(T, Acc ++ [H], Level).
+
+-spec get_module(string()) -> atom().
+get_module(FileName) ->
+    erlang:list_to_atom(filename:basename(FileName, ".erl")).
+
+
