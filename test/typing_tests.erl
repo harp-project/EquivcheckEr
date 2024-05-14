db8cc4713601cc4d3f9ef1d3de11f82462f06f63
Bendeguz Seres <grafitpor@gmail.com>
2024-05-14 07:19:38 +0200

    Add tests for typing

--- test/typing_tests.erl
+++ test/typing_tests.erl
@@ -7,39 +7,18 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-source() ->
-"-module(test3).
--compile(export_all). % Exports all functions
-
-i(Xs,X) -> test2:g(Xs) + test2:h(X).
-
--spec p() -> integer().
-p(Xs) -> 
-    test:f_new(Xs) - 4.
-
--spec q(list(integer())) -> integer().
-q(Xs) -> 
-    X = 4,
-    test:f_new(Xs) + X.
-
--spec r(integer(),string()) -> integer().
-r(X) ->
-    test:f_new([X]) + 4.".
-
-specs() ->
+valid_specs() ->
     ["-spec p() -> integer().",
      "-spec q(list(integer())) -> integer().",
-     "-spec r(integer(),string()) -> integer()."].
+     "-spec r(integer(),string()) -> integer()."
+    ].
 
-get_specs_test() ->
-  ?assertEqual(specs(), get_specs(source())).
+expected_specs() ->
+    [
+     {p, []},
+     {q, ["list(integer())"]},
+     {r, ["integer()", "string()"]}
+    ].
 
-get_args_test_() ->
-  [?_assertEqual([], get_args(source(), "p", 0)),
-   ?_assertEqual(["list(integer())"], get_args(source(), "q", 1)),
-   ?_assertEqual(["integer()","string()"], get_args(source(), "r", 2))].
-
-parse_spec_test_() ->
-  [?_assertEqual({"p", []}, parse_spec("-spec p() -> integer().")),
-   ?_assertEqual({"q", ["list(integer())"]}, parse_spec("-spec q(list(integer())) -> integer().")),
-   ?_assertEqual({"r", ["integer()","string()"]}, parse_spec("-spec r(integer(),string()) -> integer()."))].
+parse_spec_test() ->
+    ?assertEqual(expected_specs(), lists:map(fun typing:parse_spec/1, valid_specs())).
